c7246effed417a5d216058bcbee79e5f6b81f171
Tyler Rudman <tyler.rudman@gmail.com>
2023-01-22 14:55:44 -0500

    debugged seeds and db info

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ('Salesperson', 80000, 1),
+    ('Sales Manager', 125000, 1),
     ('Lead Engineer', 150000, 2),
     ('Software Engineer', 120000, 2),
     ('Account Manager', 160000, 3),
@@ -17,11 +17,11 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Cecil', 'Gardner', 1, 1),
+    ('Cecil', 'Gardner', 1, NULL),
     ('Lori', 'Freeman', 2, NULL),
     ('Austin', 'Terry', 3, 2),
     ('Shelly', 'Tate', 4, NULL), 
-    ('Julio', 'Owens', 5, 3), 
+    ('Julio', 'Owens', 5, 4), 
     ('Thomas', 'Gregory', 6, NULL),
-    ('Dora', 'Hudson', 7, 4);
+    ('Dora', 'Hudson', 7, 6);
 
