9d6ed15f457b4c80b50eeb1b939c06c27216b093
Tyler Rudman <tyler.rudman@gmail.com>
2023-01-22 11:17:30 -0500

    created schema and seeded db

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department (name)
+VALUES 
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 200000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Cecil', 'Gardner', 1, 1),
+    ('Lori', 'Freeman', 2, NULL),
+    ('Austin', 'Terry', 3, 2),
+    ('Shelly', 'Tate', 4, NULL), 
+    ('Julio', 'Owens', 5, 3), 
+    ('Thomas', 'Gregory', 6, NULL),
+    ('Dora', 'Hudson', 7, 4);
+
